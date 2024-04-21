bebd36fb8817105ed5286ceebaf4ee00e1362b6c
Jesiel Pfeifer <jesiel.pfeifer@gmail.com>
2024-04-21 02:11:11 -0300

    Adicionado background do primeiro level com animações

--- MountainShooter/code/Const.py
+++ MountainShooter/code/Const.py
@@ -1,6 +1,7 @@
 # C
-COLOR_ORANGE: (255, 128, 0)
-COLOR_WHITE: (255, 128, 0)
+COLOR_ORANGE = (255, 128, 0)
+COLOR_WHITE = (255, 255, 255)
+COLOR_YELLOW = (255, 255, 128)
 
 # M
 MENU_OPTION = ("NEW GAME 1P",
@@ -11,3 +12,16 @@ MENU_OPTION = ("NEW GAME 1P",
 # W
 WIN_WIDTH = 576
 WIN_HEIGHT = 324
+
+# i
+IMAGE_LEVEL = {'Level1Bg': 7}
+
+# E
+ENTITY_SPEED = {'Level1Bg0': 0,
+                'Level1Bg1': 1,
+                'Level1Bg2': 2,
+                'Level1Bg3': 3,
+                'Level1Bg4': 4,
+                'Level1Bg5': 5,
+                'Level1Bg6': 6,
+                }
--- MountainShooter/code/Menu.py
+++ MountainShooter/code/Menu.py
@@ -1,7 +1,9 @@
+import sys
+
 import pygame.image
 from pygame import Surface, Rect
 from pygame.font import Font
-from code.Const import WIN_WIDTH, WIN_HEIGHT, COLOR_ORANGE, MENU_OPTION
+from code.Const import WIN_WIDTH, WIN_HEIGHT, COLOR_ORANGE, MENU_OPTION, COLOR_WHITE, COLOR_YELLOW
 
 
 class Menu:
@@ -14,17 +16,35 @@ class Menu:
         pygame.mixer_music.load('./asset/Menu.mp3')
         pygame.mixer_music.play(-1)
         pygame.mixer_music.set_volume(0.1)
+        menu_option = 0
         while True:
             self.window.blit(source=self.surf, dest=self.rect)
             self.menu_text(50, "Mountain", COLOR_ORANGE, ((WIN_WIDTH / 2), 70))
-            self.menu_text(50, "Shooter", COLOR_ORANGE, ((WIN_WIDTH / 2), 70))
-            pygame.display.flip()
+            self.menu_text(50, "Shooter", COLOR_ORANGE, ((WIN_WIDTH / 2), 120))
 
             for i in range(len(MENU_OPTION)):
-                pass
-                
-
-
+                if i == menu_option:
+                    self.menu_text(20, MENU_OPTION[i], COLOR_YELLOW, (WIN_WIDTH / 2, 200 + 30 * i))
+                else:
+                    self.menu_text(20, MENU_OPTION[i], COLOR_WHITE, (WIN_WIDTH / 2, 200 + 30 * i))
+            pygame.display.flip()
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_DOWN:
+                        if menu_option < len(MENU_OPTION) - 1:
+                            menu_option += 1
+                        else:
+                            menu_option = 0
+                    if event.key == pygame.K_UP:
+                        if menu_option > 0:
+                            menu_option -= 1
+                        else:
+                            menu_option = len(MENU_OPTION) - 1
+                    if event.key == pygame.K_RETURN:
+                        return MENU_OPTION[menu_option]
 
     def menu_text(self, text_size: int, text: str, text_color: tuple, text_center_pos: tuple):
         text_font: Font = pygame.font.SysFont(name="Lacida Sans Typewriter", size=text_size)
